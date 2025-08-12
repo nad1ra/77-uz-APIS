aecc3f76df0a942263196c260340ca42d31e0569
nodira <nodirakamaliddinovna@gamil.com>
2025-08-12 16:05:50 +0500

    common's openapi_schema is complete

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import PageListView, PageDetailView, RegionWithDistrictsView, AppInfoView
 
+
 urlpatterns = [
     path('pages/', PageListView.as_view()),
     path('pages/<slug:slug>/', PageDetailView.as_view()),
--- apps/common/views.py
+++ apps/common/views.py
@@ -7,6 +7,8 @@ from .serializers import (
     AppInfoSerializer
 )
 from .pagination import CustomPagination
+from drf_yasg.utils import swagger_auto_schema
+from . import openapi_schema as schemas
 
 
 class PageListView(generics.ListAPIView):
@@ -14,20 +16,36 @@ class PageListView(generics.ListAPIView):
     serializer_class = PageListSerializer
     pagination_class = CustomPagination
 
+    @swagger_auto_schema(responses={200: schemas.page_list_response})
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 
 class PageDetailView(generics.RetrieveAPIView):
     queryset = Page.objects.all()
     serializer_class = PageDetailSerializer
     lookup_field = 'slug'
 
+    @swagger_auto_schema(responses={200: schemas.page_detail_response})
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 
 class RegionWithDistrictsView(generics.ListAPIView):
     queryset = Region.objects.prefetch_related('districts').all()
     serializer_class = RegionSerializer
 
+    @swagger_auto_schema(responses={200: schemas.region_with_districts_response})
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 
 class AppInfoView(generics.RetrieveAPIView):
     serializer_class = AppInfoSerializer
+
+    @swagger_auto_schema(responses={200: schemas.setting_response})
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
     def get_object(self):
         return AppInfo.objects.first()
